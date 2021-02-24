d59915df5aac81d92fb32cff990762b1d000b920
Alex Stroot <alexrs.business@outlook.com>
2021-02-23 23:51:49 -0600

    removed invoked nested class since it didnt like that

--- Lexicon.WPF.AttachableProperties/Window.cs
+++ Lexicon.WPF.AttachableProperties/Window.cs
@@ -4,20 +4,17 @@ namespace Lexicon.WPF.AttachableProperties
 {
     public static class Window
     {
-        public static class Invoke
+        public static readonly DependencyProperty InvokeCloseProperty = DependencyProperty.RegisterAttached("InvokeClose", typeof(bool), typeof(Window), new PropertyMetadata(OnInvokeCloseChanged));
+        public static bool GetInvokeClose(DependencyObject obj) => (bool)obj.GetValue(InvokeCloseProperty);
+        public static void SetInvokeClose(DependencyObject obj, bool value) => obj.SetValue(InvokeCloseProperty, value);
+        private static void OnInvokeCloseChanged(object sender, DependencyPropertyChangedEventArgs args)
         {
-            public static readonly DependencyProperty CloseProperty = DependencyProperty.RegisterAttached("Close", typeof(bool), typeof(Window), new PropertyMetadata(OnCloseChanged));
-            public static bool GetClose(DependencyObject obj) => (bool)obj.GetValue(CloseProperty);
-            public static void SetClose(DependencyObject obj, bool value) => obj.SetValue(CloseProperty, value);
-            private static void OnCloseChanged(object sender, DependencyPropertyChangedEventArgs args)
+            if (sender is System.Windows.Window window)
             {
-                if (sender is System.Windows.Window window)
+                if (args.NewValue is bool result)
                 {
-                    if (args.NewValue is bool result)
-                    {
-                        if (result)
-                            window.Close();
-                    }
+                    if (result)
+                        window.Close();
                 }
             }
         }
