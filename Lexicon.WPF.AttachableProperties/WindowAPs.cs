acb099e2de6b4cb65fd0c47689b0dce49d6fcb3f
Alex Stroot <alexrs.business@outlook.com>
2021-02-23 23:31:40 -0600

    updated naming of window quit to close to better reflect its action

--- Lexicon.WPF.AttachableProperties/WindowAPs.cs
+++ Lexicon.WPF.AttachableProperties/WindowAPs.cs
@@ -4,17 +4,20 @@ namespace Lexicon.WPF.AttachableProperties
 {
     public static class WindowAPs
     {
-        public static readonly DependencyProperty QuitProperty = DependencyProperty.RegisterAttached("Quit", typeof(bool), typeof(WindowAPs), new PropertyMetadata(OnQuitChanged));
-        public static bool GetQuit(DependencyObject obj) => (bool)obj.GetValue(QuitProperty);
-        public static void SetQuit(DependencyObject obj, bool value) => obj.SetValue(QuitProperty, value);
-        private static void OnQuitChanged(object sender, DependencyPropertyChangedEventArgs args)
+        public static class Invoke
         {
-            if (sender is Window window)
+            public static readonly DependencyProperty CloseProperty = DependencyProperty.RegisterAttached("Close", typeof(bool), typeof(WindowAPs), new PropertyMetadata(OnCloseChanged));
+            public static bool GetClose(DependencyObject obj) => (bool)obj.GetValue(CloseProperty);
+            public static void SetClose(DependencyObject obj, bool value) => obj.SetValue(CloseProperty, value);
+            private static void OnCloseChanged(object sender, DependencyPropertyChangedEventArgs args)
             {
-                if (args.NewValue is bool result)
+                if (sender is Window window)
                 {
-                    if (result)
-                        window.Close();
+                    if (args.NewValue is bool result)
+                    {
+                        if (result)
+                            window.Close();
+                    }
                 }
             }
         }
