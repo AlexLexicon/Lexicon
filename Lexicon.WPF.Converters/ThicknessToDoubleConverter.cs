d12f18b3e5658e5ae3d3ee85e5ab53fac53620bc
Alex Stroot <alexrs.business@outlook.com>
2021-02-27 00:29:20 -0600

    updated .net versions and added new converter

---
+++ Lexicon.WPF.Converters/ThicknessToDoubleConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace Lexicon.WPF.Converters
+{
+    public class ThicknessToDoubleConverter : LexiconConverter
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Thickness thickness)
+            {
+                double total = thickness.Left + thickness.Top + thickness.Right + thickness.Bottom;
+                return total / 4;
+            }
+            return 0;
+        }
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null)
+                return null;
+            bool success = double.TryParse(value.ToString(), out double stroke);
+            return success ? new Thickness(stroke) : (object)new Thickness();
+        }
+    }
+}
